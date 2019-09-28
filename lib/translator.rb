3f8c7dab5a55fc31cf58f22d9b57222bce3b23a6
Gee-Wey (Oct 7, London) Yue <geewey@gmail.com>
2019-09-28 08:19:01 +0000

    Done.

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,13 +1,20 @@
 # require modules here
+require "yaml"
+require "pry"
 
 def load_library
   # code goes here
+    
+  emoticons =
+  YAML.load_file(emoticons.yml)
+    
+  emoticons
 end
-
+  
 def get_japanese_emoticon
   # code goes here
 end
-
+  
 def get_english_meaning
   # code goes here
 end
