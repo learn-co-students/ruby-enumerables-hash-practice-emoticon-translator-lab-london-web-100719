b6addea7943e25d9cc1bc7f6a6f97848216e9bae
harmonious-monitor-8469 <a.shomalistos@gmail.com>
2019-10-03 13:49:14 +0000

    Done.

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,13 +1,35 @@
-# require modules here
+require "yaml"
 
-def load_library
-  # code goes here
+
+def load_library(p)
+  
+  emoties = YAML.load_file(p)
+  
+  newh = {
+    'get_meaning' => {},
+    'get_emoticon' => {}
+  }
+
+  emoties.map do |key,value|
+    newh['get_emoticon'][value[0]] = emoties[key][1]
+    newh['get_meaning'][value[1]] = key
+    
+  end
+
+  newh
+  
 end
 
-def get_japanese_emoticon
-  # code goes here
+def get_japanese_emoticon(p, emotie)
+  
+  result = load_library(p)['get_emoticon'][emotie]
+  result ? result : "Sorry, that emoticon was not found"
+  
 end
 
-def get_english_meaning
-  # code goes here
+def get_english_meaning(p, emotie)
+  
+  result = load_library(p)['get_meaning'][emotie]
+  result ? result : "Sorry, that emoticon was not found"
+  
 end
